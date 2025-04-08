1ef9702c23ca0db430f4b4c7cb2bf7ebe4599934
Zakaria <khaddanzakaria7@gmail.com>
2025-04-08 16:13:49 +0100

    first commit auth : Configuration de Gateway et Discovry

--- Discovery-services/src/main/java/obdemo/discoveryservices/DiscoveryServicesApplication.java
+++ Discovery-services/src/main/java/obdemo/discoveryservices/DiscoveryServicesApplication.java
@@ -2,8 +2,10 @@ package obdemo.discoveryservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServicesApplication {
 
     public static void main(String[] args) {
--- Gatway-services/src/main/java/obdemo/gatwayservices/GatwayServicesApplication.java
+++ Gatway-services/src/main/java/obdemo/gatwayservices/GatwayServicesApplication.java
@@ -2,12 +2,24 @@ package obdemo.gatwayservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatwayServicesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatwayServicesApplication.class, args);
+    }
+    @Bean
+    public DiscoveryClientRouteDefinitionLocator locator(
+            ReactiveDiscoveryClient discoveryClient,
+            DiscoveryLocatorProperties properties) {
+        return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
     }
 
 }
